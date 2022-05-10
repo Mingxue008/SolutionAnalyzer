97b1273dff7703ee413cd8c63f8525ff06a329ac
Mingxue Zhang <mingxzh25@hotmail.com>
2022-05-10 11:56:04 -0700

    Unit test coverage initial change

---
+++ SolutionAnalyzer.Test/CompareCodelyzer.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using NUnit.Framework;
+using System.IO;
+
+namespace SolutionAnalyzer
+{
+    [TestClass]
+    public class CompareCodelyzer
+    {
+        private string targetSolutionFolder = @"D:\work\TestProjects";
+
+        [TestInitialize]
+        public void SetUp()
+        {
+            Analyzer.RegisterVisualStudioInstance(@"C:\Program Files\Microsoft Visual Studio\2022\Community");
+        }
+
+        [TestMethod]
+        public void Roslyn_Analyze_Against_Codelyzer()
+        {
+            var targetSolutionFiles = Directory.EnumerateFiles(targetSolutionFolder, "*.sln", SearchOption.AllDirectories);
+
+            foreach(var targetSolutionFile in targetSolutionFiles)
+            {
+                var solutionResult = Analyzer.AnalyzeSolution(targetSolutionFile).Result;
+            }
+        }
+    }
+}
